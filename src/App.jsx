84eabbac74fe5e6731f091901542875a349bfb3a
onlyeugene <eugeneokogun@gmail.com>
2025-10-24 11:24:50 +0100

    feat: designed the home page and the sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import './App.css';
+// import Home from "./Home/Home";
+// import Quiz from "./QuizPage";
+// import About from './About';
+// import Stats from './Stats';
+// import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+    Home goes here
+      <Router>
+        {/* <Route exact path="/" component={Home} />
+        <Route path="/quiz" component={Quiz} /> 
+        <Route path="/about" component={About} />
+        <Route path="/stats" component={Stats} /> */}
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
